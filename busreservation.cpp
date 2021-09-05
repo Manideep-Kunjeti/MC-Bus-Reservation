fd1ba48ce023188c820df82521534e1f2e08cba5
Charan MC <57934156+CharanMC-cyb@users.noreply.github.com>
2021-09-05 22:16:09 +0530

    BusReservation.cpp
    added Welcome message and reading users choice

---
+++ busreservation.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+  int n,choice;
+  string name;
+  cout<<"Welcome to MCBus,Bus Tickets booking Platform<<endl<<Journey for your dreams"<<endl;
+  cout<<"\t\t Services offered\n 1.Book a ticket\t 2.Get Ticket Details \n 3.Ticket Cancellation\t\nchoose your choice;
+  cin>>choice
+  
+  cout<<"Please enter your name";
+  cin>>name;
+  
+  
+  
+  
+  return 0;
+}  
+
+
+
