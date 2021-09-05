e24321bbfcc29f06c4d332475262dcacf9148599
Charan MC <57934156+CharanMC-cyb@users.noreply.github.com>
2021-09-05 22:31:44 +0530

    added book function

--- busreservation.cpp
+++ busreservation.cpp
@@ -4,18 +4,36 @@ int main()
 {
   int n,choice;
   string name;
+  //welcome page
   cout<<"Welcome to MCBus,Bus Tickets booking Platform<<endl<<Journey for your dreams"<<endl;
-  cout<<"\t\t Services offered\n 1.Book a ticket\t 2.Get Ticket Details \n 3.Ticket Cancellation\t\nchoose your choice;
-  cin>>choice
+  cout<<"\t\t Services offered\n 1.Book a ticket\t 2.Get Ticket Details \n 3.Ticket Cancellation\t 4.Manage your bookings\nchoose your choice;
+  cin>>choice;
+  switch(choice)
+  case 1 : book()
+    break;
+  case 2 : details()
+    break;
+  case 3 :  cancel()
+    break;
+  case 4:  manage()
+    break;
   
-  cout<<"Please enter your name";
-  cin>>name;
-  
-  
-  
-  
-  return 0;
+    return 0;
 }  
+
+
+void book()
+{ int date,month,year;
+  string from,to,name,mob;
+  cout<<"Please enter your name and mobile number";
+  cin>>name>>mob;
+  cout<<"From: "; cin>>from;
+  cout<<"\tTo: "; cin>>to;
+  cout<<"Date of Journey: " cin<<date<<month<<year;
+
+}
+  
+  
 
 
 
